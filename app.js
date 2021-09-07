2522747809d47c5d325eec01c2cbf3f4c054b149
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2021-09-07 16:14:04 -0400

    test

--- app.js
+++ app.js
@@ -56,8 +56,7 @@ app.use('/', contact);
 // /articles/:year?/:month?/:day?
 app.use('/json/:oracle?/:tracker?/:base?/:pairs?', function (req, res, next) {
   console.log("req.params")
-  console.log(req.params)
-  var client_passport = { 
+  req.client_passport = { 
     oracle: req.params.oracle ? req.params.oracle : '',
     tracker:  req.params.tracker ? req.params.tracker : '',
     base:  req.params.base ? req.params.base : '',

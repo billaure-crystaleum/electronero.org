f910dee83a3c24818b137d3ede8af6551207520f
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2021-09-07 16:29:36 -0400

    test

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use('/', contact);
 // basically you can use the query operator and the parameters are optional 
 // https://stackoverflow.com/questions/41736413/multiple-optional-route-parameters-in-express
 // /articles/:year?/:month?/:day?
-app.use('/json/:oracle?/:tracker?/:base?/:pairs?', function (req, res, next) {
+app.use('/json/:oracle?/:tracker?/:pairs?/:from?-:to?', function (req, res, next) {
   console.log("req.params")
    
   req.coin_config = {

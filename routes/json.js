ade4ae488e27f74d2082f29cb7881f2e19770e9d
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2021-09-07 15:33:03 -0400

    test

--- routes/json.js
+++ routes/json.js
@@ -1349,7 +1349,7 @@ router.get('/oracle/:tracker/pairs/:from-:to', (req, res, next) => {
   console.log(request_mini_apps);
   console.log("req.params: ");
   console.log("\n");
-  console.log(req.params);
+  console.log(req.params); 
   const currency_pairs = [];
   const req_params_to = req.params.to;
   var currency_arr = req_params_to.toString().split(",");

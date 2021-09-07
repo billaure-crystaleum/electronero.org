424f8a90206446653887b9ab414cbe2c80c883f1
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2021-09-07 16:12:54 -0400

    test

--- app.js
+++ app.js
@@ -50,16 +50,19 @@ app.use('/privacy', legal); // add privacy route
 // app.use('/routeIwantToVisit', REQUIREDROUTE); // something useful...
 app.use('/hardfork', hardfork); // add hardfork route 
 app.use('/', contact);
-// json API route
+
 // basically you can use the query operator and the parameters are optional 
 // https://stackoverflow.com/questions/41736413/multiple-optional-route-parameters-in-express
 // /articles/:year?/:month?/:day?
-app.use('/json/:oracle?/:tracker?/:pairs?/:pairs?', function (req, res, next) {
-  console.log("client_passport")
-  req.client_passport = { 
-    oracle: req.params.tracker ? req.params.tracker : '',
-    pairs:  req.params.pairs ? req.params.pairs : ''
-  }; console.log(client_passport);
+app.use('/json/:oracle?/:tracker?/:base?/:pairs?', function (req, res, next) {
+  console.log("req.params")
+  console.log(req.params)
+  var client_passport = { 
+    oracle: req.params.oracle ? req.params.oracle : 'oracle',
+    tracker:  req.params.tracker ? req.params.tracker : 'coingecko',
+    base:  req.params.base ? req.params.base : 'pairs',
+    pairs:  req.params.pairs ? req.params.pairs : 'btc-usdt,ltc,eth' 
+  } console.log(client_passport)
   
   req.coin_config = {
     name: 'Electronero',

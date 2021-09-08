f7cb4700422631e2649efe51a838773478f61736
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2021-09-08 16:40:37 -0400

    test

--- routes/json.js
+++ routes/json.js
@@ -1397,8 +1397,8 @@ router.get('/oracle/:tracker/:from-:to', (req, res, next) => {
     var vs_coins = to_all.replace(/,/g, percent2).toString();
     console.log("vs_coins");
     console.log(vs_coins);
-    let api_to_call = 'https://api.coingecko.com/api/v3/simple/price?ids=crystaleum&vs_currencies=btc%2Cusd%2Ceth%2Cltc';
-    axios.get(api_to_call.toString()).then((response) => {
+    let api_to_call = 'https://api.coingecko.com/api/v3/simple/price?ids=crystaleum&vs_currencies=usd%2eth%2btc';
+    axios.get('https://api.coingecko.com/api/v3/simple/price?ids=crystaleum&vs_currencies=btc%2Cusd%2Ceth%2Cltc').then((response) => {
         try {
           let resp = response.data;
           var r_serialized = circularJSON.stringify(resp);
